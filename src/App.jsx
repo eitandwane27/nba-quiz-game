206e07f711987bc4fd0f527e3882b5547021a6ae
eitandwane27 <yuukimichi27@gmail.com>
2026-01-21 01:07:11 +0800

    refactor logic quiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Quiz } from "./components/Quiz";
 export default function App() {
   const [score, setScore] = useState(0);
   const [wrong, setWrong] = useState(0);
+  const [questionIndex, setQuestionIndex] = useState(0);
 
   const questions = [
     {
@@ -12,18 +13,29 @@ export default function App() {
       choices: ["Lakers", "Heat", "Nuggets", "Bucks"],
       answer: "Nuggets",
     },
+
+    {
+      question: "Who is the greatest 3 pt. shooter",
+      choices: ["Curry", "Heat", "Nuggets", "Bucks"],
+      answer: "Curry",
+    },
   ];
 
   return (
     <>
+      {/* for the ui */}
       <GameHeader
         score={score}
         setScore={setScore}
         mistakes={wrong}
-        questions={0}
+        questions={questionIndex}
+        setQuestionIndex={setQuestionIndex}
       />
+      {/* for the logic */}
       <Quiz
         questions={questions}
+        setQuestionIndex={setQuestionIndex}
+        questionIndex={questionIndex}
         score={score}
         setScore={setScore}
         setWrong={setWrong}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,33 +1,38 @@
-export const Quiz = ({ questions, setScore, setWrong }) => {
+export const Quiz = ({
+  questions,
+  setScore,
+  setWrong,
+  questionIndex,
+  setQuestionIndex,
+}) => {
   function handleAnswerClick(ch, index, rightAnswer) {
     console.log(ch, index);
 
-    if (ch === rightAnswer) {
+    if (ch === currentQuestion.answer) {
       console.log("correct");
 
       setScore((prev) => prev + 1);
+      setQuestionIndex((prev) => prev + 1);
     } else {
       setWrong((prev) => prev + 1);
+      setQuestionIndex((prev) => prev + 1);
     }
   }
+
+  const currentQuestion = questions[questionIndex]; //questions[0]
 
   return (
     <>
       <div className="main-container">
-        {questions.map((box, index) => (
-          <div key={index}>
-            {box.question}
-            <div>
-              {box.choices.map((ch, index) => (
-                <button
-                  key={index}
-                  onClick={() => handleAnswerClick(ch, index, box.answer)}
-                >
-                  {ch}
-                </button>
-              ))}
-            </div>
-          </div>
+        <h2>{currentQuestion.question}</h2>
+
+        {currentQuestion.choices.map((ch, index) => (
+          <button
+            key={index}
+            onClick={() => handleAnswerClick(ch, currentQuestion.answer)}
+          >
+            {ch}
+          </button>
         ))}
       </div>
     </>
